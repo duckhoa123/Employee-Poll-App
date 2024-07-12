92df4f3c71adf3b092af367261acb761138c1364
duckhoa123 <nguyenduckhoa1812@gmail.com>
2024-07-13 09:54:12 +1000

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const data = require('./utils/_DATA.js');
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('_saveQuestion', () => {
+  it('will return the saved question if correctly formatted data is passed to the function', async() => {
+    
+    const question = {
+      optionOneText: "Work more time",
+      optionTwoText: "Work less time",
+      author: "david",
+    };
+    const savedQuestion = await data._saveQuestion(question);
+    expect(question.optionOneText).toEqual(savedQuestion.optionOne.text);
+    expect(question.optionTwoText).toEqual(savedQuestion.optionTwo.text);
+    expect(question.author).toEqual(savedQuestion.author);
+  });
+
+  it("will reject with an error message if incorrect data is passed in", async () => {
+    const question = {
+      optionOneText: "foo",
+      optionTwoText: "bar",
+    };
+
+    await expect(data._saveQuestion(question)).rejects.toEqual(
+      "Please provide optionOneText, optionTwoText, and author"
+    );
+  });
 });
